2db0205a166dc5f8a9285daeb8e57fddccd62d44
Gareth Fleming <me@d3z.me>
2018-10-01 21:20:24 +0100

    Parse script files into structs.

---
+++ README.md
@@ -0,0 +1,34 @@
+# Scripted Server
+## A scriptable HTTP server for client testing
+
+This project is an 'itch scratcher'. Some time ago, I ran into an issue with a misbehaving server that would regularly throw a 404 for the same request. It appeared to happen every 3 or 4 requests, or after a request to another path (I can't remember the exact details). I had no control over the server, but my client application was not handling the situation very well.
+
+Because I couldn't change the server, it made sense for me to write a mock server that basically mimicked the bad behaviour and throw a 404 after the 3 request.
+
+From this, I had idea for a more generalised, scriptable server that I could used to setup some server-side scenario like the one above in order to test that client applications handled them correctly.
+
+Imagine you have an application that makes a request for some resource then uses some property of that resource to make another call to the service for a different resource. What happens if, for whatever reason, the server responds with a 404 on the second request, but only sometimes?
+
+### Using the scripted server
+Write a script file (more on that below) and run `scripted_server script_file` where `script_file` is the path to the script file on your computer.
+
+### The script file
+A script file specifies the requests and responses the server will make in order that it expects the client to call them. The server will follow these steps, matching requests from clients with the current step and returning the configured response, advancing to the next step and waiting to the next request from the client.
+
+Here is an example script file with an explanation of how the server will use it.
+
+```yaml
+name: My first script file # used for logging
+loop: true # should the server loop over the steps or end, defaults to false
+path: /api/contact # the path that all steps match on by default
+steps:
+    - name: OK # used for logging
+      code: 200 # HTTP response code returned by this step
+      content: "ok" # body of reponse to return (can also be path to a file)
+      content-type: "text/plain" # defaults to "text/plain"
+      repeat: 2 # how many times to use this step, defaults to 1
+    - name: Error # used for logging
+      path: /api/contact/123 # overrides the global path
+      code: 404
+```
+
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 extern crate structopt;
+extern crate yaml_rust;
 
-use std::io::prelude::*;
 use std::fs::File;
-use std::path::PathBuf;
+use std::io::prelude::*;
+use std::path::{Path, PathBuf};
 use structopt::StructOpt;
+use yaml_rust::{YamlLoader, Yaml};
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "reqs")]
@@ -12,9 +14,85 @@ struct Opt {
     script_file: PathBuf
 }
 
+#[derive(Debug, Clone)]
+struct Step {
+    name: String,
+    code: i64,
+    content: String,
+    content_type: String
+}
+
+#[derive(Debug, Clone)]
+struct Script {
+    name: String,
+    repeat: bool,
+    path: String,
+    current_step: i32,
+    steps: Vec<Step>
+}
+
+static TEXT_PLAIN: &str = "text/plain";
+
+fn get_content_from_file(file_path: &str) -> String {
+    let mut contents = String::new();
+    let mut script_file = File::open(file_path).expect("Could not open response file");
+    script_file.read_to_string(&mut contents).expect("Could not read response file");
+    return contents;
+}
+
+fn get_content(content_str: &str) -> String {
+    match Path::new(content_str).exists() {
+        true => get_content_from_file(content_str),
+        _ => String::from(content_str)
+    }
+}
+
+fn parse_step(step_definition: &Yaml) -> Vec<Step> {
+    println!("{:?}", step_definition);
+    let times = match step_definition["times"].as_i64() { Some(times) => times as usize, None => 1 };
+    let mut steps = Vec::with_capacity(times);
+    let step = Step {
+        name: String::from(step_definition["name"].as_str().unwrap()),
+        code: step_definition["code"].as_i64().unwrap(),
+        content: match step_definition["content"].as_str() { Some(content) => get_content(content), None => String::from("") },
+        content_type: match step_definition["content_type"].as_str() { Some(content_type) => String::from(content_type), None => String::from(TEXT_PLAIN) },
+    };
+    for _ in 0..times {
+        steps.push(step.clone());
+    }
+    println!("{:?}", steps);
+    return steps;
+}
+
+fn parse_steps(step_definitions: &Vec<Yaml>) -> Vec<Step> {
+    let mut steps = Vec::new();
+    step_definitions.into_iter().for_each(|step_definition| parse_step(step_definition).into_iter().for_each(|step| steps.push(step)));
+    return steps;
+}
+
+fn get_path_from_script(path: Option<&str>) -> String {
+    match path {
+        Some(path_str) => String::from(path_str),
+        None => String::from("")
+    }
+}
+
+fn parse_script(script_str: &str) -> Script {
+    let script = &YamlLoader::load_from_str(script_str).unwrap()[0];
+    Script {
+        name: String::from(script["name"].as_str().unwrap()),
+        repeat: script["repeat"].as_bool().unwrap(),
+        path: get_path_from_script(script["path"].as_str()),
+        current_step: 0,
+        steps: parse_steps(script["steps"].as_vec().unwrap())
+    }
+}
+
 fn main() {
     let opt = Opt::from_args();
     let mut script_contents = String::new();
     let mut script_file = File::open(opt.script_file).expect("Could not open script file");
     script_file.read_to_string(&mut script_contents).expect("Could not read script file");
+    let script = parse_script(&script_contents);
+    println!("{:?}", script);
 }
